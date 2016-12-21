7434df4f7795051505fdec9e3aedfd1e4e7f5c70
amacshines <nekromanter20000@gmail.com>
2016-12-21 16:32:37 +0200

    init

---
+++ DownloadFileConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceUrl = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+            string urlDownloadedFile = @"G:\image.png";
+            DownloadManager downloadManager = new DownloadManager();
+            downloadManager.DownloadFile(sourceUrl, urlDownloadedFile);
+            Console.ReadKey();
+        }               
+    }
+
+    public class DownloadManager
+    {
+        public void DownloadFile(string sourceUrl, string targetUrl)
+        {
+            WebClient downloader = new WebClient();            
+            // fake as if you are a browser making the request.
+            downloader.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+            downloader.DownloadFileCompleted += new AsyncCompletedEventHandler(Downloader_DownloadFileCompleted);
+            downloader.DownloadProgressChanged +=
+                new DownloadProgressChangedEventHandler(Downloader_DownloadProgressChanged);
+            downloader.DownloadFileAsync(new Uri(sourceUrl), targetUrl);
+            // wait for the current thread to complete, since the an async action will be on a new thread.
+            while (downloader.IsBusy) { }
+        }
+        private void Downloader_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            //print progress of download.
+            Console.WriteLine(e.BytesReceived + " " + e.ProgressPercentage);
+        }
+        private void Downloader_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            //display completion status.
+            if (e.Error != null)
+                Console.WriteLine(e.Error.Message);
+            else
+                Console.WriteLine("Download completed!!!");
+        }
+    }
+}
